bfed1f1d0b13765a27edaf08d93c4dd4a8a9d2cd
Vitor Luiz Ramalho <vitorlramalho@gmail.com>
2023-04-03 14:15:04 -0300

    commit

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", function(req, res) {
+    Response.send("<h1>Hello World</h1>");
+});
+
 app.listen(8000, function() {
-    console.log("Server atarted on port 8000")
+    console.log("Server atarted on port 8000");
 });
