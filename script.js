9544145e60814089fcfd25095992dd039d4312be
Vitor Luiz Ramalho <vitorlramalho@gmail.com>
2023-04-03 16:06:16 -0300

    add express

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ app.get("/", function(req, res) {
     Response.send("<h1>Hello World</h1>");
 });
 
-app.listen(8000, function() {
-    console.log("Server atarted on port 8000");
+app.listen(3000, function() {
+    console.log("Server atarted on port 3000");
 });
