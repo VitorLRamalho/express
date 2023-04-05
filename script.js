481387157c2f82308ce85e3ef8e76001070a1801
Vitor Luiz Ramalho <vitorlramalho@gmail.com>
2023-04-05 11:10:20 -0300

    commit

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res) {
-    res.send("<h1>Hello World</h1>");
+    res.send("<h1>Hello, World!</h1>");
 });
 
-app.listen(3000, function() {
-    console.log("Server atarted on port 3000");
+app.listen(8081, function() {
+    console.log("Server started on http://localhost:8081/");
 });
